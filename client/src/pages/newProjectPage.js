61647715ffd269a22421767e851a0e3a572d938f
Holly Hanley <hollykhanley@yahoo.co.uk>
2022-03-14 16:56:15 +0000

    eop commit views included

---
+++ client/src/pages/newProjectPage.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+
+export default function newProjectPage() {
+  return (
+    <div>newProjectPage</div>
+  )
+}
