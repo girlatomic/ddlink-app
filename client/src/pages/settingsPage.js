fb3140440e52a963de3db474bf253a619c08bf40
Meena Haque <aneemqah@gmail.com>
2022-03-15 10:33:42 +0000

    fix path names

--- client/src/pages/settingsPage.js
+++ client/src/pages/settingsPage.js
@@ -2,7 +2,5 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function SettingsPage() {
-  return (
-    <div>settingsPage</div>
-  )
+  return <div>settingsPage</div>;
 }
