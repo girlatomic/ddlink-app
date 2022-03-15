93b8a03cb83b24c4333d4ed0244c6db59630d96b
Holly Hanley <hollykhanley@yahoo.co.uk>
2022-03-15 10:16:50 +0000

    fixed bugs in views

--- client/src/pages/mainPage.js
+++ client/src/pages/mainPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-export default function mainPage() {
+export default function MainPage() {
   return (
     <div>mainPage</div>
   )
