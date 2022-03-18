009a0a6a2b32ba27357c9ea928bfcd25324c0cd3
Meena Haque <aneemqah@gmail.com>
2022-03-18 14:07:53 +0000

    swap out dummy data with real DB data

--- client/src/pages/ProjectCard.js
+++ client/src/pages/ProjectCard.js
@@ -1,27 +1,30 @@
-import React, { useState, useMemo, useRef } from "react";
+import React, { useState, useMemo, useRef, useEffect } from "react";
 import TinderCard from "react-tinder-card";
 import "./ProjectCard.css";
 
 function ProjectCard() {
-  const [projects, setProjects] = useState([
-    {
-      name: "Project 1",
-      url: "https://images.unsplash.com/photo-1647357681882-4c87933e6c74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor justo quis ornare dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor justo quis ornare dictum",
-    },
-    {
-      name: "Project 2",
-      url: "https://images.unsplash.com/photo-1606473506347-49181771312f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor justo quis ornare dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor justo quis ornare dictum",
-    },
-    {
-      name: "Project 3",
-      url: "https://images.unsplash.com/photo-1616677001238-454f4c7d2ccd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
-      desc: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor justo quis ornare dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor justo quis ornare dictum",
-    },
-  ]);
-  const [currentIndex, setCurrentIndex] = useState(projects.length - 1);
+  const [projects, setProjects] = useState([]);
+  console.log("I AM PROJECTS", projects);
+
+  const [currentIndex, setCurrentIndex] = useState(); //projects.length - 1
   const [lastDirection, setLastDirection] = useState();
+
+  const getProjects = () => {
+    fetch("/projects")
+      .then((response) => response.json())
+      .then((projects) => {
+        setProjects(projects);
+        setCurrentIndex(projects.length - 1); //!!!!!!
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
   // used for outOfFrame closure
   const currentIndexRef = useRef(currentIndex);
 
@@ -30,7 +33,7 @@ function ProjectCard() {
       Array(projects.length)
         .fill(0)
         .map((i) => React.createRef()),
-    []
+    [projects] // remembers the last index memo!
   );
 
   const updateCurrentIndex = (val) => {
@@ -45,6 +48,8 @@ function ProjectCard() {
   const swiped = (direction, nameToDelete, index) => {
     setLastDirection(direction);
     updateCurrentIndex(index - 1);
+    // setCurrentIndex(index - 1);
+    // setCanGoBack(currentIndex < projects.length - 1);
   };
 
   const outOfFrame = (name, idx) => {
@@ -67,25 +72,25 @@ function ProjectCard() {
 
   return (
     <div id="root-container">
-      <div class="sub-container">
+      <div className="sub-container">
         <h1>DDLink</h1>
         <div className="cardContainer">
           {projects.map((project, index) => (
             <TinderCard
+              key={project.id}
               ref={childRefs[index]}
               className="swipe"
-              key={project.name}
-              onSwipe={(dir) => swiped(dir, project.name, index)}
-              onCardLeftScreen={() => outOfFrame(project.name, index)}
+              onSwipe={(dir) => swiped(dir, project.p_name, index)}
+              onCardLeftScreen={() => outOfFrame(project.p_name, index)}
               preventSwipe={["up", "down"]}
             >
               <div
-                style={{ backgroundImage: "url(" + project.url + ")" }}
+                style={{ backgroundImage: "url(" + project.p_img + ")" }}
                 className="cards"
               >
-                <h3>{project.name}</h3>
-                <div className="card-text">
-                  <div className="card-body">{project.desc}</div>
+                <h3>{project.p_name}</h3>
+                <div className="sub-card-container">
+                  <div className="sub-card-text">{project.p_description}</div>
                 </div>
               </div>
             </TinderCard>
