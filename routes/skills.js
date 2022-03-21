a1c18635e5514ab2f7625712043de4ad02beb19c
Gaby Pineda <gabygabypineda@gmail.com>
2022-03-21 19:50:14 +0100

    projects and skills many to many db model

--- routes/skills.js
+++ routes/skills.js
@@ -2,31 +2,104 @@ var express = require("express");
 var router = express.Router();
 const db = require("../model/helper");
 
-// GET all skills
-router.get("/", async (req, res) => {
+
+/**
+ * Guards
+ **/
+
+
+ async function ensureSkillExists(req, res, next) {
   try {
-    let results = await db("SELECT * FROM skills");
-    let skills = results.data;
-    res.send(skills);
+      let results = await db(`SELECT * FROM skills WHERE id = ${req.params.id}`);
+      if (results.data.length === 1) {
+          // Skill was found; save it in response obj for the route function to use
+          res.locals.skill = results.data[0];
+          // Let next middleware function run
+          next();
+      } else {
+          res.status(404).send({ error: 'Skill not found' });
+      }
   } catch (err) {
-    res.status(500).send({ error: err.message });
+      res.status(500).send({ error: err.message });
+  }
+}
+
+/**
+ * Helpers
+ **/
+
+
+ async function sendAllSkills(res) {
+  // We don't need try/catch here because we're always called from within one
+  let results = await db('SELECT * FROM skills');
+  res.send(results.data);
+}
+
+
+// Convert DB results into a useful JSON format: author obj with nested array of book objs
+function joinToJson(results) {
+  // Get first row
+  let row0 = results.data[0];
+
+  // Create array of project objs
+  let projects = [];
+  if (row0.projectId) {
+      projects = results.data.map(p => ({
+          id: p.projectId,
+          p_name: p.p_name,
+          p_description: p.p_description,
+          p_img: p.p_img
+      }));
+  }
+
+  // Create skill obj
+  let skill = {
+      id: row0.skillId,
+      s_role: row0.s_role,
+      skill_name: row0.skill_name,
+      projects
+  };
+
+  return skill;
+}
+
+
+
+/**
+ * Routes
+ **/
+
+// GET all skills
+router.get('/', async function(req, res) {
+  try {
+      sendAllSkills(res);
+  } catch (err) {
+      res.status(500).send({ error: err.message });
   }
 });
 
-// GET skill by Id
-router.get("/:id", async (req, res) => {
-  let skillsId = req.params.id;
+// GET skill by ID
+router.get('/:id', ensureSkillExists, async function(req, res) {
+  // If we get here we know the author exists (thanks to guard)
+  let skill = res.locals.skill;
 
   try {
-    let results = await db(`SELECT * FROM skills WHERE id = ${skillsId}`);
-    let skills = results.data;
-    if (skills.length === 0) {
-      res.status(404).send({ error: "Skills not found" });
-    } else {
-      res.send(skills[0]);
-    }
+      // Get skill; use LEFT JOIN to also return projects
+      let sql = `
+          SELECT s.*, p.*, s.id AS skillId, p.id AS projectId
+          FROM skills AS s
+          LEFT JOIN projects_skills AS ps ON s.id = ps.skillId
+          LEFT JOIN projects AS p ON ps.projectId = p.id
+          WHERE s.id = ${skill.id}
+      `;
+
+      let results = await db(sql);
+      // Convert DB results into "sensible" JSON
+      author = joinToJson(results);
+
+      res.send(author);
   } catch (err) {
-    res.status(500).send({ error: err.message });
+      res.status(500).send({ error: err.message });
   }
 });
 
