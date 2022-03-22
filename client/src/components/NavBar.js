e2cc6a06a88582ab0655b6dfaed2cfc594b45bf3
Gaby Pineda <gabygabypineda@gmail.com>
2022-03-22 11:59:09 +0100

    adds styling to home page and logo to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,7 +6,7 @@ export default function NavBar(props) {
     <div>
         <nav className="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
         <div className="container-fluid">
-            <Link to="/" className="navbar-brand" href="#">DDLink</Link>
+            <Link to="/" className="navbar-brand" href="#"><img src="/img/logo-white.svg" className="logo"/></Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
