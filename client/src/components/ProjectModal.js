c3c636699b51831ba1fb016db9faa6369aaec169
Gaby Pineda <gabygabypineda@gmail.com>
2022-05-12 15:31:54 +0200

    adds change of backgroud and buttons color

--- client/src/components/ProjectModal.js
+++ client/src/components/ProjectModal.js
@@ -1,39 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import ProjectsTable from '../pages/ProjectsTable'; 
+import ProjectsTable from "../pages/ProjectsTable";
 
 export default function ProjectModal(props) {
-
-    return (
-
+  return (
     <div className="container-fluid">
       <div className="FeaturedProject" id={props.project.id}>
-      <div className="modal mt-5 pt-5" tabIndex="-1" style={{ display: 'block' }}>
-        <div className="modal-dialog mt-5 pt-5 ">
-        <div className="modal-content mt-5">
-        <div className="modal-header">
-        <h5 className="modal-title">{props.project.p_name}</h5>
-        <button type="button" className="btn-close" onClick={props.hide}></button>
+        <div
+          className="modal mt-5 pt-5"
+          tabIndex="-1"
+          style={{ display: "block" }}
+        >
+          <div className="modal-dialog mt-5 pt-5 ">
+            <div className="modal-content mt-5 border border-primary border-1 bg-dark text-light">
+              <div className="modal-header border border-primary">
+                <h5 className="modal-title fs-4">{props.project.p_name}</h5>
+                <button
+                  type="button"
+                  className="btn-close btn-close-white"
+                  onClick={props.hide}
+                ></button>
+              </div>
+              <div className="modal-body">
+                <div className="container-fluid text-center text-wrap">
+                  <img
+                    className="float-start me-2"
+                    style={{ width: "200px" }}
+                    src={props.project.p_img}
+                    alt=""
+                  />
+                  <h5>{props.project.p_description}</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="modal-body">
-      
-      <div className="container-fluid text-center text-wrap">
-          <img className="float-start me-2" style={{ width: '200px' }} src={props.project.p_img} alt="" />
-          <h5>{props.project.p_description}</h5>
-      </div>
-
     </div>
-    
-   </div>
-   </div>
-   </div>
-      
-  
-     
-         
-
-  </div>
-  </div>
 
     // <div className="FeaturedProject">
     //     <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
@@ -43,7 +46,7 @@ export default function ProjectModal(props) {
     //     <div className="modal-dialog">
     //     <div className="modal-content">
     //     <div className="modal-header">
-    //     <h5 className="modal-title">Example</h5> 
+    //     <h5 className="modal-title">Example</h5>
     //     {/* <button type="button" className="btn-close" onClick={props.hide}></button> */}
     //     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     //     <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -60,6 +63,5 @@ export default function ProjectModal(props) {
     // </div>
 
     // </div>
-
-  )
+  );
 }
