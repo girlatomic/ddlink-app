286b3d894323f1d8e76b4b43561afa89700cc8c5
Gaby Pineda <gabygabypineda@gmail.com>
2022-05-20 17:17:12 +0200

    adds multi-select skill form

--- client/src/pages/SkillsForm.js
+++ client/src/pages/SkillsForm.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Select from "react-select";
 
 export default function SkillsForm() {
+  let [skills, setSkills] = useState([]);
+
+  let options = skills.map((skill) => {
+    return {
+      label: skill.skill_name,
+      value: skill.id,
+    };
+  });
+
+  useEffect(() => {
+    getSkills();
+  }, []);
+
+  const getSkills = () => {
+    fetch("/skills")
+      .then((response) => response.json())
+      .then((skills) => {
+        setSkills(skills);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="container">
       <h2 className="title">Edit your skills</h2>
+      <div>
+        <Select
+          isMulti
+          name="skills"
+          options={options}
+          className="basic-multi-select"
+          classNamePrefix="select"
+        />
+      </div>
     </div>
   );
 }
