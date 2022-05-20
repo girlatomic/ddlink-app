102993ec74220e3830afa3949ffa7fab0ace9817
Gaby Pineda <gabygabypineda@gmail.com>
2022-05-20 12:53:23 +0200

    aligns form buttons

--- client/src/pages/ProjectsTable.js
+++ client/src/pages/ProjectsTable.js
@@ -1,8 +1,7 @@
-import React, {useState, useEffect} from "react";
-import './ProjectsTable.css';
-import Local from '../helpers/Local';
+import React, { useState, useEffect } from "react";
+import "./ProjectsTable.css";
+import Local from "../helpers/Local";
 import { Link, useNavigate } from "react-router-dom";
-
 
 function ProjectsTable(props) {
   let navigate = useNavigate();
@@ -10,11 +9,11 @@ function ProjectsTable(props) {
 
   const getProjects = () => {
     fetch("/projects")
-      .then(response => response.json())
-      .then(projects => {
+      .then((response) => response.json())
+      .then((projects) => {
         setProjects(projects);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   };
@@ -24,69 +23,83 @@ function ProjectsTable(props) {
   }, []);
 
   function handleClick(event) {
-        console.log(event.target.value);
-        event.preventDefault();
-        deleteProject(event.target.value);
-        navigate(`/users/${props.user.id}`);
+    console.log(event.target.value);
+    event.preventDefault();
+    deleteProject(event.target.value);
+    navigate(`/users/${props.user.id}`);
   }
 
   async function deleteProject(project) {
-        let options = {
-          method: "DELETE",
-          headers: { "Content-Type": "application/json" }
-        };
-    
-        try {
-          let id = project;
-          let response = await fetch(`/projects/${id}`, options);
-          if (response.ok) {
-            let projects = await response.json();
-            getProjects(projects);
-          } else {
-            console.log(`Server error: ${response.status} ${response.statusText}`);
-          }
-        } catch (err) {
-          console.log(`Server error: ${err.message}`);
-        }
+    let options = {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    };
+
+    try {
+      let id = project;
+      let response = await fetch(`/projects/${id}`, options);
+      if (response.ok) {
+        let projects = await response.json();
+        getProjects(projects);
+      } else {
+        console.log(`Server error: ${response.status} ${response.statusText}`);
+      }
+    } catch (err) {
+      console.log(`Server error: ${err.message}`);
+    }
   }
 
   return (
     <div className="container">
-        <div className="d-flex justify-content-between align-items-center border-bottom mt-5 mb-5">
-        <h2>My Projects</h2><Link to={`/newprojectpage`} className="btn btn-primary">
-         + Add a project
+      <div className="d-flex justify-content-between align-items-center border-bottom mt-5 mb-5">
+        <h2>My Projects</h2>
+        <Link to={`/newprojectpage`} className="btn btn-primary">
+          + Add a project
         </Link>
-        </div>
-        <div className="table-responsive-lg">
-          <table className="table-responsive">
-              <thead>
-              <tr>
-                  <th>id</th>
-                  <th>Project Name</th>
-                  <th>Description</th>
-                  <th>Image</th>
-                  <th>Actions</th>
+      </div>
+      <div className="table-responsive-lg">
+        <table className="table-responsive">
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>Project Name</th>
+              <th>Description</th>
+              <th>Image</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {projects.map((p) => (
+              <tr key={p.id}>
+                <td>{p.id}</td>
+                <td>{p.p_name}</td>
+                <td className="ps-0 pt-2">{p.p_description}</td>
+                <td>
+                  <img src={p.p_img} />
+                </td>
+                <td className="colspan-2">
+                  <Link
+                    to={`/editprojectpage/${p.id}`}
+                    class="btn btn-outline-primary me-2 mt-2"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    type="button"
+                    className="btn btn-outline-danger mt-2"
+                    value={p.id}
+                    onClick={handleClick}
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
-              </thead>
-              <tbody>
-              {projects.map(p => (
-                  <tr key={p.id}>
-                      <td>{p.id}</td>
-                      <td>{p.p_name}</td>
-                      <td className="ps-0 pt-2">{p.p_description}</td>
-                      <td><img src={p.p_img}/></td>
-                      <td className="colspan-2">
-                      <Link to={`/editprojectpage/${p.id}`} class="btn btn-outline-primary">Edit</Link>
-                      <button type="button" className="btn btn-outline-danger mt-2" value={p.id} onClick={handleClick}>Delete</button>
-                      </td>
-                  </tr>
-                  ))
-              }
-              </tbody>
+            ))}
+          </tbody>
         </table>
-       </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ProjectsTable;
