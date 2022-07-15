858b264ad518f961f23998bd4145807207257f20
Gaby Pineda <gabygabypineda@gmail.com>
2022-07-15 16:14:07 +0200

    reorganizes login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,62 +1,47 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 // import { Routes, Route, useNavigate } from 'react-router-dom';
-import './Login.css';
-import GoogleLogin from 'react-google-login';
+import "./Login.css";
+import GoogleLogin from "react-google-login";
+import GoogleButton from "react-google-button";
 
 function Login(props) {
-
   const handleFailure = (result) => {
     alert(result);
   };
 
   return (
     <div>
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-              <div className="card border-1 shadow rounded-3 my-5">
-                <div className="card-body p-4 p-sm-5">
-                  <h5 className="card-title text-center mb-5 fw-light fs-5">Sign in</h5>
-    
-                  <form>
-                    <div className="form-floating mb-3">
-                      <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                      <label htmlFor="floatingInput">Email address</label>
-                    </div>
-                    <div className="form-floating mb-3">
-                      <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-                      <label htmlFor="floatingPassword">Password</label>
-                    </div>
-
-                    <div className="form-check mb-3">
-                      <input className="form-check-input" type="checkbox" value="" id="rememberPasswordCheck"/>
-                      <label className="form-check-label" htmlFor="rememberPasswordCheck">
-                        Remember password
-                      </label>
-                    </div>
-                    <div className="d-grid">
-                      <button className="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Login</button>
-                    </div>
-                    <hr className="my-4"/>
-                    <div className="d-grid mb-2">
-                      <GoogleLogin
-                        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                        prompt="select_account"
-                        buttonText="Sign in with Google"
-                        // className="ct-button ct-button--secondary"
-                        onSuccess={props.googleLogin}
-                        onFailure={handleFailure}
-                        cookiePolicy="single_host_origin"
-                      />
-                    </div>
-                  </form>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div className="card text-center border-primary border-1 bg-dark text-light shadow rounded-3 mt-5 pt-5">
+              <div className="card-body p-4 p-sm-5">
+                <h5 className="card-title text-center mb-5">
+                  Sign in with your Google Account
+                </h5>
+                <div className="d-flex justify-content-center mb-5">
+                  <GoogleLogin
+                    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                    prompt="select_account"
+                    render={(renderProps) => (
+                      <GoogleButton
+                        onClick={renderProps.onClick}
+                        disabled={renderProps.disabled}
+                        label="Continue with Google"
+                      ></GoogleButton>
+                    )}
+                    onSuccess={props.googleLogin}
+                    onFailure={handleFailure}
+                    cookiePolicy="single_host_origin"
+                  />
                 </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
